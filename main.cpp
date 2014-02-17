116eb27f350b3bcd39e2f55cfa816d492531e483
JGreig1995 <greigjarrett@outlook.com>
2014-02-17 13:51:22 -0800

    Create main.cpp
    // This is just a small program to get used to using a public repository on GitHub.
    // You know, your basic "Hello World!"

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// This is just a small program to get used to using a public repository on GitHub.
+// You know, your basic "Hello World!"
+
+int main()
+{
+  cout << endl << "Hello World!" << endl;
+  return 0;
+}
