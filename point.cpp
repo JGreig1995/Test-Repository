2db5b05d9d660b2e5d5a6a2a77a2399e60f6ed9f
JGreig1995 <greigjarrett@outlook.com>
2014-02-19 23:37:11 -0800

    Create point.cpp
    Began the coding of point.cpp.
    
    point.cpp contains the implementations of the methods including in the point class. Check point.h for documentation.

---
+++ point.cpp
@@ -0,0 +1,86 @@
+// File point.cpp
+// This file contains the implementation of methods of the point class.
+// CLASS: three dimensional point (see point.h) for documentation.
+
+#include <iostream>
+#include <cmath>
+#include "point.h"
+using namespace std;
+
+namespace Jarrett_Greig
+{
+  point::point(float initx, float inity, float initz)
+  //Precondition: A point object is created.
+  //Postcondition: The point is set by default to have 0 for all coordinates.
+  {
+    x = 0;
+    y = 0;
+    z = 0;
+  }
+  
+  void point::shift_x(float amount)
+  //Precondition: A point has an existing x coordinate.
+  //Postcondition: The point's x coordinate is shifted by amount.
+  {
+    x += amount;
+  }
+  
+  void point::shift_y(float amount)
+  //Precondition: A point has an existing y coordinate.
+  //Postcondition: The point's y coordinate is shifted by amount.
+  {
+    y += amount;
+  }
+  
+  void point::shift_z(float amount)
+  //Precondition: A point has an existing z coordinate.
+  //Postcondition: The point's z coordinate is shifted by amount.
+  {
+    z += amount;
+  }
+  
+  void point::rotate_x(float a)
+  //Precondition: A point has x, y, z coordinates.
+  //Postcondition: The point is rotated a degrees about x axis.
+  {
+    float yprime, zprime;
+    yprime = (y * cos( a * 3.14159 / 180.0)) - (z * sin( a * 3.14159 / 180.0));
+    zprime = (y * sin( a * 3.14159 / 180.0)) + (z * cos( a * 2.14159 / 180.0));
+    y = yprime;
+    z = zprime;
+  }
+  
+  void point::rotate_y(float a)
+  //Precondition: A point has x, y, z coordinates.
+  //Postcondition: The point is rotated a degrees about y axis.
+  {
+    float xprime, zprime;
+    xprime = (x * cos( a * 3.14159 / 180.0)) + (z * sin( a * 3.14159 / 180.0));
+    zprime = (z * cos( a * 3.14159 / 180.0)) - (x * sin( a * 3.14159 / 180.0));
+    x = xprime;
+    z = zprime;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
